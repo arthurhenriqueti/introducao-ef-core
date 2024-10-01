92ef2ce402420b438cd8510b6a4eb2c5db2b7661
Arthur <arthurbleckrep2@gmail.com>
2024-10-01 17:35:09 -0300

    Adicionando as aulas do módulo 4

---
+++ Aulas/Fundamentos/PrimeiroProjeto/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Entity Framework Core");
+
+// Code First: Classes (ORM) -> Banco de dados (Tabelas)
+// Database First: Banco de dados (Tabelas) -> Classes (Scaffolding)
+// DbContext: Classe principal do pacote Entity Framework Core (Add, Update, Remove, SaveChanges)
